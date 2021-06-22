991f053b974fb29135330448ba1488e3d0e7548a
Vladzo <vashchukvlad17@gmail.com>
2021-06-23 01:14:43 +0300

    Final

--- index.js
+++ index.js
@@ -2,34 +2,48 @@ const fs = require('fs');
 const path = require('path');
 
 //1) Студентів з 1800 перевести в групу на 2000. І навпаки
-// const removeFiles = (from, to) => {
-//     fs.readdir(from, (err, files) => {
-//         if (err) {
-//             console.log("////////////////////////////////");
-//             console.log(err);
-//             console.log("////////////////////////////////");
-//             return err;
-//         }
-//
-//         files.forEach(fileName => {
-//             fs.rename(path.join(from, fileName), path.join(to, fileName), err => {
-//                 if (err) {
-//                     console.log("////////////////////////////////");
-//                     console.log(err);
-//                     console.log("////////////////////////////////");
-//                     return err;
-//                 }
-//             });
-//         });
-//     })
-// }
-//
-// removeFiles(path.join(__dirname, "1800"), path.join(__dirname, "2000"));
-// removeFiles(path.join(__dirname, "2000"), path.join(__dirname, "1800"));
+const removeFiles = (from, to) => {
+    fs.readdir(from, (err, files) => {
+        if (err) {
+            console.log("////////////////////////////////");
+            console.log(err);
+            console.log("////////////////////////////////");
+            return err;
+        }
+
+        files.forEach(fileName => {
+            fs.rename(path.join(from, fileName), path.join(to, fileName), err => {
+                if (err) {
+                    console.log("////////////////////////////////");
+                    console.log(err);
+                    console.log("////////////////////////////////");
+                    return err;
+                }
+            });
+        });
+    })
+}
+
+removeFiles(path.join(__dirname, "1800"), path.join(__dirname, "2000"));
+removeFiles(path.join(__dirname, "2000"), path.join(__dirname, "1800"));
 
 
 
 // 2) Перемістити всіх дівчат в папку girls а хлопців в папаку boys.
+// fs.mkdir(path.join(__dirname, 'boys'), {recursive: true}, err => {
+//     if(err) {
+//         console.log(err);
+//         return;
+//     }
+// })
+// fs.mkdir(path.join(__dirname, 'girls'), {recursive: true}, err => {
+//     if(err) {
+//         console.log(err);
+//         return;
+//     }
+// })
+//
+//
 // const pathToGirls = path.join(__dirname, "girls");
 // const pathToBoys = path.join(__dirname, "boys");
 //
@@ -92,71 +106,3 @@ const path = require('path');
 //
 // //flat all files in directory 1
 // flat(path.join(pathDir, "1"));
-
-
-
-//*****************************************************************Backup*************
-fs.mkdir(path.join(__dirname, '1800'), {recursive: true}, err => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-})
-
-fs.mkdir(path.join(__dirname, '2000'), {recursive: true}, err => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-})
-
-const users = ["Karina", "Oleg", "Kate", "Vlad", "Yulia", "Victor"];
-
-fs.writeFile(path.join(__dirname, '1800', 'Karina.txt'), '{"gender": "female"}', err => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-})
-
-fs.writeFile(path.join(__dirname, '1800', 'Oleg.txt'), '{"gender": "male"}', err => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-})
-
-fs.writeFile(path.join(__dirname, '1800', 'Karina.txt'), '{"gender": "female"}', err => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-})
-
-fs.writeFile(path.join(__dirname, '1800', 'Kate.txt'), '{"gender": "female"}', err => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-})
-
-fs.writeFile(path.join(__dirname, '2000', 'Vlad.txt'), '{"gender": "male"}', err => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-})
-
-fs.writeFile(path.join(__dirname, '2000', 'Yulia.txt'), '{"gender": "female"}', err => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-})
-
-fs.writeFile(path.join(__dirname, '2000', 'Victor.txt'), '{"gender": "male"}', err => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-})
